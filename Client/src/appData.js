4afd4566ff290fae0aff38917854c8252562a73f
orya <orya.sp@gmail.com>
2022-10-23 10:24:14 +0300

    Added init and getRecipes functions to appData.js

---
+++ Client/src/appData.js
@@ -0,0 +1,26 @@
+/* Data Model
+This model is in charge of handling all data related functionalities, it should make all the API's request 
+and able to generate a user’s data.  */
+
+const data = function() {
+
+    let recipes;
+    
+    function init(ingredient, glutenFree, dairyFree) {
+        recipes = new MetaDataApi(ingredient, glutenFree, dairyFree)
+    }
+
+    async function getRecipes() {
+        let recipesPromise = recipes.getData()
+        return await Promise.all([recipesPromise]).then(function(res) {
+            recipes = res[0]
+            return {recipes: res[0]}
+        })
+    }
+
+
+    return {
+        init, 
+        getRecipes
+    }
+}
